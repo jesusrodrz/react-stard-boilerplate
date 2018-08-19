4d66516fb2da43d89219c8d41c2fed890da46ccf
Jesus Rodriguez <jesus.rodrz97@gmail.com>
2018-08-19 17:02:23 -0400

    react app basic structure

---
+++ src/js/components/index.jsx
@@ -0,0 +1,22 @@
+import React,{ Component, ReactPropTypes } from 'react';
+import './../sass/styles.scss'
+
+class App extends Component {
+  contructor(...props) {
+    super(...props)
+
+    
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>React App</div>
+    )  
+  }
+  
+}
+
+export default App
