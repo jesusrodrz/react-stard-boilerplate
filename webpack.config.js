4ae6127d5ec2d3270bd8592b304c43069779a0e4
Jesus Rodriguez <jesus.rodrz97@gmail.com>
2018-08-19 16:19:45 -0400

    inical commit

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+// webpack v4
+const path = require('path')
+const views = require('./webpack.helper.js')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  stats: {
+    modules: false,
+    colors: true,
+    version: false,
+    children: false
+  },
+  entry: {
+    index: './src/js/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: path.resolve(__dirname, 'dist/css')
+            }
+          },
+          'css-loader',
+          'sass-loader'
+        ]
+
+      },
+      {
+        test: /\.html$/,
+        use: [
+          'html-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+    }),
+    ...views.pages({
+      srcDir: './src',
+      filesExt:'html'
+    }) 
+  ]
+};
